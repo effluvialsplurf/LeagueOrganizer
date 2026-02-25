4e0bb0590e97c8aa89e7eabc7c395d9bcbf70326
eeso <eesowrk@gmail.com>
2026-02-24 22:04:07 -0500

    made a bunch of pages

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Layout from "./Layout.tsx";
 import NotFound from "./NotFound.tsx";
+import Bracket from "./pages/Bracket.tsx";
+import League from "./pages/League.tsx";
+import Settings from "./pages/Settings.tsx";
+import Standings from "./pages/Standings.tsx";
+import Stats from "./pages/Stats.tsx";
 
 export default function App() {
 
@@ -11,11 +16,16 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="bracket" element={<Bracket />} />
+          <Route path="league" element={<League />} />
+          <Route path="standings" element={<Standings />} />
+          <Route path="stats" element={<Stats />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
 
         {/* 404 */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   )
 }
--- frontend/app/src/NavBar.tsx
+++ frontend/app/src/NavBar.tsx
@@ -7,6 +7,21 @@ export default function NavBar() {
         <Link to="/" className="text-white hover:text-blue-300  ml-3">
           League Organizer
         </Link>
+        <Link to="/bracket" className="text-white hover:text-blue-300 ml-3">
+          Bracket
+        </Link>
+        <Link to="/League" className="text-white hover:text-blue-300 ml-3">
+          League
+        </Link>
+        <Link to="/Standings" className="text-white hover:text-blue-300 ml-3">
+          Standings
+        </Link>
+        <Link to="/Stats" className="text-white hover:text-blue-300 ml-3">
+          Stats
+        </Link>
+        <Link to="/Settings" className="text-white hover:text-blue-300 ml-3">
+          Settings
+        </Link>
       </div>
     </div >
   );
